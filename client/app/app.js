06de43a51c19a4dcdb896fd724997a682b550a05
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-12-01 17:57:05 -0500

    setup routes

--- client/app/app.js
+++ client/app/app.js
@@ -12,5 +12,16 @@ function($stateProvider,$urlRouterProvider,$httpProvider){
       url: '/Welcome',
       templateUrl: 'app/welcome/welcome.html',
       controller: 'WelcomeController'
+    })
+    .state('users', {})
+    .state('users.user', {
+      url: '/users/:userCode',
+      templateUrl: 'app/users/user/user.html',
+      controller: 'UserController'
+    })
+    .state('users.user.courses', {
+      url: '/users/:userCode/courses',
+      templateUrl: 'app/users/user/courses/courses.html',
+      controller: 'CoursesController'
     });
 }]);
--- specs/client/routingSpec.js
+++ specs/client/routingSpec.js
@@ -11,4 +11,16 @@ describe('Routing', function() {
     expect($state.get('welcome').controller).to.be('WelcomeController');
     expect($state.get('welcome').templateUrl).to.be('app/welcome/welcome.html');
   });
+
+  it('should have a user state, template, and controller nested under users', function(){
+    expect($state.get('users.user')).to.be.ok();
+    expect($state.get('users.user').controller).to.be('UserController');
+    expect($state.get('users.user').templateUrl).to.be('app/users/user/user.html');
+  });
+
+  it('should have a courses state, template, and controller nested under user', function(){
+    expect($state.get('users.user.courses')).to.be.ok();
+    expect($state.get('users.user.courses').controller).to.be('CoursesController');
+    expect($state.get('users.user.courses').templateUrl).to.be('app/users/user/courses/courses.html');
+  });
 });
