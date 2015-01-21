f5c3e304d164a9d80e1f2f26c72725d79bfda4bf
drabinowitz <dmitrirabinowitz+development@gmail.com>
2015-01-21 13:23:02 -0500

    Update github callback

--- server/helpers/passport-helper.js
+++ server/helpers/passport-helper.js
@@ -47,7 +47,7 @@ var passportHelper = function(app){
   passport.use(new GithubStrategy({
     clientID: GITHUB.CLIENT_ID,
     clientSecret: GITHUB.CLIENT_SECRET,
-    callbackURL: 'http://127.0.0.1:8000/github/callback'
+    callbackURL: 'https://mysterious-depths-9268.herokuapp.com/github/callback'
   },
   function(accessToken,refreshToken, profile, done){
     if (profile.username === 'Guest'){return;}
